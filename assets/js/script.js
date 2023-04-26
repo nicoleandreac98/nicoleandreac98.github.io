631ee984343521d435d83b56b557135259ddefe4
nicoleandreac98 <nicole_030790@hotmail.com>
2023-04-26 01:18:31 -0400

    ultimo detalles de javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function(){
     // Selecciona todos los enlaces que apuntan a un id en la misma página
-    $('a[href*="#"]').click(function(event) {
+  $('a[href*="#"]').click(function(event) {
       // Evita el comportamiento por defecto de los enlaces
-        event.preventDefault();
+      event.preventDefault();
 
       // Obtiene el id del elemento al que el enlace apunta
-        var target = $(this).attr("href");
+      var target = $(this).attr("href");
 
       // Usa animate() para hacer el smooth scroll
-        $('html, body').animate({
-        scrollTop: $(target).offset().top
-      }, 3000); // El número indica la duración de la animación en milisegundos
-    });
+      $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 3000); // El número indica la duración de la animación en milisegundos
+  });
 
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')) 
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) { return new bootstrap.Tooltip(tooltipTriggerEl) 
-    });
-
-
+  document.getElementById("miFormulario").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previene el envío automático del formulario
+    if (confirm("¿Estás seguro de que deseas enviar el formulario?")) {
+      // Aquí va el código que se ejecutará si el usuario hace clic en "Aceptar"
+      window.location.href = "https://nicoleandreac98.github.io/#inicio"; // Redirige al usuario a otra página
+    } else {
+      // Aquí va el código que se ejecutará si el usuario hace clic en "Cancelar"
+    }
+  });
 });
